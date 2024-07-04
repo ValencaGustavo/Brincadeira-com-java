c49365486fbecda70e4a98b6c636aae5732e2bad
Gustavo Valença Marcilio <workgustavo10@gmail.com>
2024-07-04 16:52:28 -0300

    Add files via upload

---
+++ Brincadeira.java
@@ -0,0 +1,14 @@
+public class Brincadeira {
+
+    public static void main(String[] args) throws Exception {
+        String varMotivacao1 = "Largue de ser egocêntrico!";
+
+        String varMotivacao2 = "Larguem de ser vagabundo invejoso!";
+
+        String varString1 = "Se você acha que os outros se importa com você a ponto de odiá-lo. Então você se acha demais! " + varMotivacao1;
+        System.out.println(varString1);
+
+        String varString2 = "As pessoas que não trabalham duro não têm o direito de invejar as pessoas com talento. Elas falham porque não entendem o trabalho árduo que é necessário para ter sucesso! " + varMotivacao2;
+        System.out.println(varString2);
+    }
+}
